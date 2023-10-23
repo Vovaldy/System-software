72b27828b9b4ff748ec8b29ecb00fe9aa7387e60
Vovaldy <raek06@mail.ru>
2023-10-23 21:50:01 +0300

    Lr2 - part1
    Сделал 2 приложения с обменом информации с помощью консоли

---
+++ Lr2/App2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Аргумент 1\n{args[0][0]}\nАргумент 2\n{args[0][1]}" +
+            $"\nРезультат:" +
+            $"\n{(Convert.ToInt32(args[0][0]) + Convert.ToInt32(args[0][1]) - 96).ToString()}");
+        
+        Thread.Sleep(1000);
+        Console.ReadLine();
+    }
+}
---
+++ Lr2/Lr2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lr2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox3.Text = (Convert.ToInt32(textBox1.Text)
+            //+ Convert.ToInt32(textBox2.Text)).ToString();
+            var process = new Process();
+
+            process.StartInfo.FileName = @"C:\Users\raek0\OneDrive\Документы\GitHub\System-software\Lr2\App2\bin\Release\net6.0\App2.exe";
+            process.StartInfo.Arguments = $"{textBox1.Text}{textBox2.Text}";
+            process.Start();
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
