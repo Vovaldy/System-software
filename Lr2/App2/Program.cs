4e2d8ad6c436d772a3dd717bb13fba58b7fc0620
Vovaldy <raek06@mail.ru>
2023-10-24 19:38:29 +0300

    Взаимодействие через общую память

--- Lr2/App2/Program.cs
+++ Lr2/App2/Program.cs
@@ -1,12 +1,47 @@
 ﻿
+using System.IO.MemoryMappedFiles;
+
 class TestClass
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"Аргумент 1\n{args[0][0]}\nАргумент 2\n{args[0][1]}" +
+
+        if (args.Length < 1)
+        {
+            //Массив для сообщения из общей памяти
+            int[] message;
+            //Размер введенного сообщения
+            int size;
+
+            //Получение существующего участка разделяемой памяти
+            //Параметр - название участка
+            MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("MemoryFile");
+            //Сначала считываем размер сообщения, чтобы создать массив данного размера
+            //Integer занимает 4 байта, начинается с первого байта, поэтому передаем цифры 0 и 4
+            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(0, 4, MemoryMappedFileAccess.Read))
+            {
+                size = reader.ReadInt32(0);
+            }
+            //Считываем сообщение, используя полученный выше размер
+            //Сообщение - это строка или массив объектов char, каждый из которых занимает два байта
+            //Поэтому вторым параметром передаем число символов умножив на из размер в байтах плюс
+            //А первый параметр - смещение - 4 байта, которое занимает размер сообщения
+            using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(4, size * 4, MemoryMappedFileAccess.Read))
+            {
+                //Массив символов сообщения
+                message = new int[size];
+                reader.ReadArray<int>(0, message, 0, size);
+            }
+            Console.WriteLine($"Аргумент1 : {message[0]}\nАргумент 2: {message[1]}\n" +
+                $"Сумма:{message[0] + message[1]}");
+        }
+        else
+        {
+            Console.WriteLine($"Аргумент 1\n{args[0][0]}\nАргумент 2\n{args[0][1]}" +
             $"\nРезультат:" +
             $"\n{(Convert.ToInt32(args[0][0]) + Convert.ToInt32(args[0][1]) - 96).ToString()}");
-        
+        }
+
         Thread.Sleep(1000);
         Console.ReadLine();
     }
--- Lr2/Lr2/Form1.cs
+++ Lr2/Lr2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +25,43 @@ namespace Lr2
             //+ Convert.ToInt32(textBox2.Text)).ToString();
             var process = new Process();
 
-            process.StartInfo.FileName = @"C:\Users\raek0\OneDrive\Документы\GitHub\System-software\Lr2\App2\bin\Release\net6.0\App2.exe";
-            process.StartInfo.Arguments = $"{textBox1.Text}{textBox2.Text}";
-            process.Start();
+            if (textBox1.Text!=null && textBox2.Text !=null)
+            {
+                process.StartInfo.Arguments = $"{textBox1.Text}{textBox2.Text}";
+                process.StartInfo.FileName = @"C:\Users\raek0\OneDrive\Документы\GitHub\System-software\Lr2\App2\bin\Release\net6.0\App2.exe";
 
+                process.Start();
+            }
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Создание участка разделяемой памяти
+            //Первый параметр - название участка, 
+            //второй - длина участка памяти в байтах: тип char  занимает 2 байта 
+            //плюс четыре байта для одного объекта типа Integer
+            int size = textBox1.Text.Length + textBox2.Text.Length;
+            int[] message = new int[2];
+            message[0] = Convert.ToInt32(textBox1.Text);
+            message[1] = Convert.ToInt32(textBox2.Text);
+            MemoryMappedFile sharedMemory = MemoryMappedFile.CreateOrOpen("MemoryFile", size * 4 + 4);
+
+            //Создаем объект для записи в разделяемый участок памяти
+            using (MemoryMappedViewAccessor writer = sharedMemory.CreateViewAccessor(0, size * 4 + 4))
+            {
+                //запись в разделяемую память
+                //запис ь размера с нулевого байта в разделяемой памяти
+                writer.Write(0, size);
+                //запись сообщения с четвертого байта в разделяемой памяти
+                writer.WriteArray<int>(4, message, 0, size);
+            }
+            var process = new Process();
+            process.StartInfo.FileName = @"C:\Users\raek0\OneDrive\Документы\GitHub\System-software\Lr2\App2\bin\Release\net6.0\App2.exe";
+
+            process.Start();
 
         }
 
@@ -35,5 +69,7 @@ namespace Lr2
         {
 
         }
+
+
     }
 }
